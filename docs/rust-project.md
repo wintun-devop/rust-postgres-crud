afba67563b8dda1498b3e874d18489f00a66d41f
wintun-devop <wintun.hlaing@outlook.com>
2025-11-08 14:06:38 +0630

    basic async setup done

--- docs/rust-project.md
+++ docs/rust-project.md
@@ -3,6 +3,11 @@
 cargo new your_porject_name
 ```
 
+### resolve missing autocomplete,diagnostics
+```
+cargo check
+```
+
 ### Add necessary packages
 - add tokio standard packages
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod utils;
+use utils::hello::say_hello;
 
-
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+// #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+#[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    println!("Welcome");
+    say_hello(1).await;
+    say_hello(2).await;
 }
